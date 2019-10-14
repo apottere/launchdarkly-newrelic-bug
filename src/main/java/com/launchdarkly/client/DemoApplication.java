86fea70a450bcc3176d04d46c2b5ba038c399924
Andrew Potter <apottere@gmail.com>
2019-10-14 17:04:17 -0400

    Remove spring-boot

--- src/main/java/com/launchdarkly/client/DemoApplication.java
+++ src/main/java/com/launchdarkly/client/DemoApplication.java
@@ -1,8 +1,5 @@
 package com.launchdarkly.client;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
 	    NewRelicReflector.annotateTransaction("", "");
